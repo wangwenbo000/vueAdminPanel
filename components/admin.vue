cc8ae3cbd1cbe4542c91a2c1e0f24fc3251d4ebb
wangwenbo <wangwenbo000@me.com>
2015-12-30 11:22:23 +0800

    开始布局

--- components/admin.vue
+++ components/admin.vue
@@ -2,21 +2,10 @@
     <Topnav></Topnav>
     <div class="container">
         <div class="row">
-            <div class="col-xs-3">
-                <div class="card slider">
-                    <div class="card-header">
-                        <h5>设定选项 <small>Setting</small></h5>
-                    </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item"><a href="">Cras justo odio</a></li>
-                        <li class="list-group-item">Dapibus ac facilisis in</li>
-                        <li class="list-group-item active">Morbi leo risus</li>
-                        <li class="list-group-item">Porta ac consectetur ac</li>
-                        <li class="list-group-item">Vestibulum at eros</li>
-                    </ul>
-                </div>
+            <div class="col-xs-2">
+                <Slider></Slider>
             </div>
-            <div class="col-xs-9">
+            <div class="col-xs-10">
                 <router-view transition="fade" transition-mode="out-in"></router-view>
             </div>
         </div>
@@ -26,21 +15,15 @@
 
 <script type="text/babel">
     import Topnav from './nav/nav.vue'
+    import Slider from './slider/slider.vue'
     export default{
         components: {
-            Topnav
+            Topnav,
+            Slider
         }
     }
 </script>
 
 <style lang="sass">
-    @import "../bootstrap/scss/bootstrap.scss";
-    .container{
-        margin-top: 66px;
-    }
-    .slider{
-        .card-header{
-            border-bottom: none;
-        }
-    }
+@import "style.scss";
 </style>
---
+++ components/slider/slider.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="list-group">
+        <a href="#" class="list-group-item active">
+            控制台
+        </a>
+        <a href="#" class="list-group-item">新闻发布</a>
+        <a href="#" class="list-group-item">视频管理</a>
+        <a href="#" class="list-group-item">平面管理</a>
+        <a href="#" class="list-group-item">团队管理</a>
+    </div>
+</template>
+
+<script type="text/babel">
+
+</script>
+
+<style lang="sass?outputStyle=expanded">
+
+</style>
