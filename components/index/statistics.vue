dd425335c2abb3b489b2a039d89516c62bd8297e
wangwenbo <wangwenbo000@me.com>
2016-01-06 10:23:21 +0800

    test command line

--- components/index/statistics.vue
+++ components/index/statistics.vue
@@ -2,25 +2,25 @@
     <div class="row">
         <div class="col-sm-3">
             <div class="card card-block card-inverse card-primary text-center">
-                <h2 class="card-text display-1">30</h2>
+                <h2 class="card-text">30</h2>
                 <p class="card-text">视频播放</p>
             </div>
         </div>
         <div class="col-sm-3">
             <div class="card card-block card-inverse card-primary text-center">
-                <h2 class="card-text display-1">30</h2>
+                <h2 class="card-text">30</h2>
                 <p class="card-text">公告</p>
             </div>
         </div>
         <div class="col-sm-3">
             <div class="card card-block card-inverse card-primary text-center">
-                <h2 class="card-text display-1">30</h2>
+                <h2 class="card-text">30</h2>
                 <p class="card-text">访问数</p>
             </div>
         </div>
         <div class="col-sm-3">
             <div class="card card-block card-inverse card-success text-center">
-                <h2 class="card-text display-1">238</h2>
+                <h2 class="card-text">238</h2>
                 <p class="card-text">服务器时限 | 从2015-10-06</p>
             </div>
         </div>
